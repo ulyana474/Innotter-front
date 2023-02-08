1e0ccbe8e1a3cd8340e0954156754fd91b85dbc3
Ulyana.Drozdova <ulyana.drozdova@innowise-group.com>
2023-02-07 17:59:32 -0800

    added follow page, rewrote responseHandlers functions into class

--- innotter-front/src/utils/responseHandlers.js
+++ innotter-front/src/utils/responseHandlers.js
@@ -2,12 +2,24 @@ import unliked_path from '../images/like.svg'
 import liked_path from '../images/liked.svg'
 
 
-export const likeHandler = (response, user) => {
-    let likes_arr = response.likes
-    let user_obj = JSON.parse(user)
-    let curr_user_id = user_obj["user"]["id"]
-    if (likes_arr.includes(curr_user_id)){
+export class ResponseHandler {
+    constructor(user) {
+        this.curr_user_id = user["user"]["id"]
+    }
+
+    checkUserInLikesArray = (response) => {
+    let array = response.likes
+    if (array.includes(this.curr_user_id)){
         return liked_path
     }
     return unliked_path
+    }
+
+    checkUserInFollowersArray = (response) => {
+        let array = response.followers
+        if (array.includes(this.curr_user_id)){
+            return "unfollow"
+        }
+        return "follow"
+        }
 }
